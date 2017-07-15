ce51362a87e0115ee6d96af092e9038a2c443229
MichalGoly <me@michalgoly.com>
2017-07-15 21:19:26 +0100

    periodically getting the current location and printing it to the console

--- app/src/main/java/com/michalgoly/mapify/ui/MapFragment.java
+++ app/src/main/java/com/michalgoly/mapify/ui/MapFragment.java
@@ -2,20 +2,35 @@ package com.michalgoly.mapify.ui;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.michalgoly.mapify.R;
 import com.michalgoly.mapify.com.michalgoly.mapify.parcels.TrackWrapper;
 
@@ -25,9 +40,18 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     private static final String TAG = "MapFragment";
 
     private static final int REQUEST_LOCATION = 1;
+    private static final long UPDATE_INTERVAL_MS = 10_000;
+    private static final long FASTEST_UPDATE_INTERVAL_MS = UPDATE_INTERVAL_MS / 2;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     private SupportMapFragment mapFragment = null;
     private GoogleMap googleMap = null;
+    private FusedLocationProviderClient fusedLocationProviderClient = null;
+    private SettingsClient settingsClient = null;
+    private LocationSettingsRequest locationSettingsRequest = null;
+    private LocationCallback locationCallback = null;
+    private LocationRequest locationRequest = null;
+    private LocationResult currentLocation = null;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -111,6 +135,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         this.googleMap = googleMap;
         this.googleMap.setOnMyLocationButtonClickListener(this);
         enableLocation();
+        initLocationServices();
     }
 
     @Override
@@ -131,6 +156,70 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         }
     }
 
+    public void initLocationServices() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        settingsClient = LocationServices.getSettingsClient(getContext());
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                /*
+                 * 1. Add the locationResult to the List of locations
+                 * 2. Update the UI to draw the path
+                 */
+                Log.d(TAG, "onLocationResult: " + locationResult.getLastLocation().toString());
+                currentLocation = locationResult;
+            }
+        };
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL_MS);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationSettingsRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        startLocationUpdates();
+    }
+
+    public void startLocationUpdates() {
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i(TAG, "All location settings are satisfied.");
+                        try {
+                            fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                                    locationCallback, Looper.myLooper());
+                        } catch (SecurityException e) {
+                            Log.wtf("Should never happen", e);
+                        }
+                    }
+                })
+                .addOnFailureListener(getActivity(), new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+                                Toast.makeText(getContext(), errorMessage, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
     /**
      * Interaction with the parent Activity
      */
@@ -144,7 +233,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
             // Permission to access the location is missing.
             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION);
-
         } else if (googleMap != null) {
             // Access to the location has been granted to the app.
             googleMap.setMyLocationEnabled(true);
