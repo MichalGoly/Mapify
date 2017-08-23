19fa7dc68bb5e2c41be88ac2f243234e1d7a05a2
MichalGoly <me@michalgoly.com>
2017-08-23 12:38:14 +0200

    added greenDAO

--- app/src/main/java/com/michalgoly/mapify/handlers/PersistenceHandler.java
+++ app/src/main/java/com/michalgoly/mapify/handlers/PersistenceHandler.java
@@ -1,7 +1,29 @@
 package com.michalgoly.mapify.handlers;
 
-public class PersistenceHandler {
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class PersistenceHandler extends Service {
+
     private static final String TAG = "PersistenceHandler";
 
+    private IBinder binder = new ServiceBinder();
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind() called");
+        return binder;
+    }
+
+    public class ServiceBinder extends Binder {
+
+        public PersistenceHandler getService() {
+            return PersistenceHandler.this;
+        }
+    }
 }
